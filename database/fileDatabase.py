90d08628983fb88bdd4a1b7f07a98db8d89b8628
Tom <tomnomnom1@gmail.com>
2015-02-04 01:31:35 +0000

    scrapped everything before this, now doing project

--- database/fileDatabase.py
+++ database/fileDatabase.py
@@ -1,9 +1,9 @@
 import redis
 
-keyFormat = 'file_{0}_{1}_{2}'
+keyFormat = 'file_{0}'
 postRedisDB = redis.StrictRedis( '127.0.0.1', 6379 )#TODO: move to config file
 
-def addFile(boardId, threadId, postId, fileId, fileInfo, creatorIP, creatorId=None):
+def addFile(fileId, fileInfo, creatorIP, creatorId=None):
 	if creatorId == None:
 		creatorId = 'NULL'
 
@@ -17,7 +17,6 @@ def addFile(boardId, threadId, postId, fileId, fileInfo, creatorIP, creatorId=No
 		'mimetype':     '',
 		'fileMetadata': fileInfo['metadata'],
 		'fileType':     fileInfo['type'],
-		'postId': 		postId,
 		'creatorId':	creatorId,
 		'creatorIP':	creatorIP
 	}
@@ -26,6 +25,6 @@ def addFile(boardId, threadId, postId, fileId, fileInfo, creatorIP, creatorId=No
 	key = keyFormat.format( boardId, threadId, fileId )
 	postRedisDB.hmset( key, fileData )
 
-def getFileInfo(boardId, threadId, postId, fileId):
-	key = keyFormat.format( boardId, threadId, fileId )
+def getFileInfo(fileId):
+	key = keyFormat.format( fileId )
 	return postRedisDB.hgetall( key )
